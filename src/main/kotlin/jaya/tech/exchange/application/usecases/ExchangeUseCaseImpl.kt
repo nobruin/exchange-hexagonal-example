93fbcaa6af2f543e61f90ddded3b01d74c803ddb
ext_brsalis <ext_brsalis@mercadolibre.com>
2023-07-10 17:53:33 -0300

    feature(add-interfaces) adding port interfaces

--- src/main/kotlin/jaya/tech/exchange/application/usecases/ExchangeUseCaseImpl.kt
+++ src/main/kotlin/jaya/tech/exchange/application/usecases/ExchangeUseCaseImpl.kt
@@ -1,29 +1,37 @@
 package jaya.tech.exchange.application.usecases
 
 import jaya.tech.exchange.adapters.output.external.exchangeapi.ExchangeGateway
+import jaya.tech.exchange.adapters.output.persistence.repositories.ExchangeRepository
+import jaya.tech.exchange.domain.Exchange
 import java.lang.RuntimeException
 import java.math.BigDecimal
 
 class ExchangeUseCaseImpl(
-    private val exchangeGateway: ExchangeGateway
+    private val exchangeGateway: ExchangeGateway,
+    private val exchangeRepository: ExchangeRepository
 ): ExchangeUseCase {
     override fun convertCurrency(amount: BigDecimal, fromCurrency: String, toCurrency: String): BigDecimal {
         // get rates
         val exchangeResult = exchangeGateway.findRates(fromCurrency, toCurrency)
+        //calculate
         val rateConversion =
             calculateConversionRate(exchangeResult.rates[fromCurrency], exchangeResult.rates[toCurrency])
-        //calculate
+        val result = calculateAmount(amount, rateConversion)
         //save
-
+        exchangeRepository.save(Exchange(
+            amount = amount,
+            fromCurrency = fromCurrency,
+            toCurrency = toCurrency,
+            result = result
+        ))
         // return
-
-        return calculateAmount(amount, rateConversion)
+        return result
     }
 
     private fun calculateConversionRate(currency: Double?, destinyCurrency: Double?): BigDecimal? {
         when{
-            currency == null || currency <= 0.0 -> throw RuntimeException("values or Values is invalid for this operation")
-            destinyCurrency == null || destinyCurrency <= 0.0 -> throw RuntimeException("values or Values is invalid for this operation")
+            currency == null || currency <= 0.0 -> throw RuntimeException(RUNTIME_EXCEPTION_MESSAGE)
+            destinyCurrency == null || destinyCurrency <= 0.0 -> throw RuntimeException(RUNTIME_EXCEPTION_MESSAGE)
         }
         return (destinyCurrency?.div(currency!!))?.toBigDecimal()
     }
@@ -33,5 +41,9 @@ class ExchangeUseCaseImpl(
             throw RuntimeException("value is invalid for this operation")
         }
         return amount.let { rateConversion.times(it) }
+    }
+
+    companion object{
+        const val RUNTIME_EXCEPTION_MESSAGE = "values or Values is invalid for this operation"
     }
 }
--- src/main/kotlin/jaya/tech/exchange/domain/Exchange.kt
+++ src/main/kotlin/jaya/tech/exchange/domain/Exchange.kt
@@ -3,7 +3,6 @@ package jaya.tech.exchange.domain
 import java.math.BigDecimal
 
 data class Exchange(
-    val id: Long? = null,
     val amount: BigDecimal,
     val fromCurrency: String,
     val toCurrency: String,
